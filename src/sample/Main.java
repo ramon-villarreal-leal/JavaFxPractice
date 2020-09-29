6f43173e1c67ccf768478c5a01db45bf535203bd
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-29 09:59:15 -0500

    updating fx app without sample.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,24 +2,24 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello Cool Cat");
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+        primaryStage.setTitle("Hello JavaFX");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-
-        primaryStage.setTitle("Hello Ghouls");
-        primaryStage.setScene(new Scene(root, 400, 500));
-        primaryStage.show();
-
-
 
     }
 
